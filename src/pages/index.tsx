89753846c34953c4dca0a2b7262875edb9b26d02
ThomOliver <thomoliver98@gmail.com>
2024-04-15 16:51:05 -0300

    Primeiro commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import { useState } from 'react';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
-  const [buttonPosition, setButtonPosition] = useState({ top: 'initial', left: 'initial', position: 'static' });
+  // Alterar a tipagem para aceitar string ou number.
+  const [buttonPosition, setButtonPosition] = useState<{ top: string | number; left: string | number; position: string }>({
+    top: 'initial',
+    left: 'initial',
+    position: 'static'
+  });
 
   const handleMouseOver = () => {
-    // Ajusta o botão "Não" para uma posição aleatória e muda para posição absoluta.
+    // Gerar nova posição aleatória para o botão "Não"
     const newPosition = {
       top: Math.random() * (window.innerHeight - 20),
       left: Math.random() * (window.innerWidth - 20),
@@ -16,16 +21,16 @@ export default function Home() {
   };
 
   const redirectToYouTube = () => {
-    window.location.href = "https://www.youtube.com";
+    window.location.href = "https://www.youtube.com/watch?v=Zn28zfPwjus";
   };
 
   return (
     <>
       <Head>
-        <title>Participe Hoje</title>
+        <title>Cuzinho</title>
       </Head>
       <div className={styles.container}>
-        <h1>Quer participar hoje?</h1>
+        <h1>Cuzinho hoje?</h1>
         <div className={styles.buttonContainer}>
           <button onClick={redirectToYouTube} className={styles.yesButton}>
             Sim
